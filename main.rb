f2a49c2f275fe9fce761deb85e4b8af829bacb60
LAPTOP-4QVU197N\KB <kibrish49@gmail.com>
2023-02-06 21:16:44 +0200

    Fix linter error

--- main.rb
+++ main.rb
@@ -2,16 +2,16 @@ require './person'
 require './student'
 require './teacher'
 
-person1 = Person.new( 54,'Kibre', parent_permission: false)
-person2 = Person.new( 17, 'Abebe',  parent_permission: true)
+person1 = Person.new(54, 'Kibre', parent_permission: false)
+person2 = Person.new(17, 'Abebe', parent_permission: true)
 
 p person1.can_use_services?
 p person2.can_use_services?
 
-student = Student.new(age: 27,classroom: 'class B')
+student = Student.new(age: 27, classroom: 'class B')
 
 p student.play_hooky
 
-teacher = Teacher.new(age: 37, specialization:'Class A')
+teacher = Teacher.new(age: 37, specialization: 'Class A')
 
 p teacher.can_use_services?
