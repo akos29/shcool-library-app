057c2ec628a0b6d01934b871a2d606c6e5fcac15
LAPTOP-4QVU197N\KB <kibrish49@gmail.com>
2023-02-08 16:22:45 +0200

    Fix linter Error

--- main.rb
+++ main.rb
@@ -12,21 +12,21 @@ require './book'
 # p capitalized_trimmed_person = TrimmerDecorator.new(capitalized_person)
 # p capitalized_trimmed_person.correct_name
 
-p class_a = Classroom.new('117 A')
-# class_b = Classroom.new('118 A')
+# p class_a = Classroom.new('117 A')
+# # class_b = Classroom.new('118 A')
 
-aster = Student.new(age: 35, name: 'Aster', classroom: class_a)
+# aster = Student.new(age: 35, name: 'Aster', classroom: class_a)
 
-p class_a.add_student(aster)
-p class_a.students.map(&:name)
+# p class_a.add_student(aster)
+# p class_a.students.map(&:name)
 
-person = Person.new(22, 'maximilianus')
-book = Book.new(title: 'Animal Farm', author: 'George Orwell')
-p book1 = Book.new(title: 'The Idiot', author: 'Fyodor Dostoyevsky')
+# person = Person.new(22, 'maximilianus')
+# book = Book.new(title: 'Animal Farm', author: 'George Orwell')
+# p book1 = Book.new(title: 'The Idiot', author: 'Fyodor Dostoyevsky')
 
-Rental.new(date: '2017-12-22', person: person, book: book)
-Rental.new(date: '2017-12-22', person: person, book: book1)
+# Rental.new(date: '2017-12-22', person: person, book: book)
+# Rental.new(date: '2017-12-22', person: person, book: book1)
 
-p person.rentals.count
-p person.rentals.map(&:date)
-p book.rentals.count
+# p person.rentals.count
+# p person.rentals.map(&:date)
+# p book.rentals.count
