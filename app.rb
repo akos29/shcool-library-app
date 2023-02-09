1a421856bb8c9dffed5aab18bf5d309d0a395bc9
LAPTOP-4QVU197N\KB <kibrish49@gmail.com>
2023-02-09 18:13:13 +0200

    Fix linter error

--- app.rb
+++ app.rb
@@ -80,7 +80,6 @@ class App
     puts 'Select a person from the following list by number (not id):'
     list_all_people
     person_choice = user_input('Your selection:   ').to_i
-    # person = @people.each_with_index{|_val,index| index==(person_choice-1)}
     person = @people[person_choice - 1]
     puts person.name
     date = user_input('Date:    ')
@@ -143,39 +142,3 @@ class App
     end
   end
 end
-
-
-# person = Person.new(22, 'maximilianus')
-# p person.correct_name
-# p capitalized_person = CapitalizeDecorator.new(person)
-# p capitalized_person.correct_name
-# p capitalized_trimmed_person = TrimmerDecorator.new(capitalized_person)
-# p capitalized_trimmed_person.correct_name
-
-# class_a = Classroom.new('117 A')
-# class_b = Classroom.new('118 A')
-
-# aster = Student.new(age: 35, name: 'Aster', classroom: class_a)
-# kebede = Student.new(age: 45, name: 'Kebede', classroom: class_b)
-
-# class_a.add_student(aster)
-# class_a.add_student(kebede)
-# class_a.students.map(&:name)
-
-# person = Person.new(22, 'maximilianus')
-# book = Book.new(title: 'Animal Farm', author: 'George Orwell')
-# p book1 = Book.new(title: 'The Idiot', author: 'Fyodor Dostoyevsky')
-
-# Rental.new(date: '2017-12-22', person: person, book: book)
-# Rental.new(date: '2017-12-22', person: person, book: book1)
-# Rental.new(date: '2017-12-22', person: aster, book: book)
-# Rental.new(date: '2017-12-22', person: kebede, book: book)
-
-# p person.rentals.count
-# p person.rentals.map(&:date)
-# p book.rentals.count
-
-# p aster.rentals.count
-# aster.rentals.map { |rental| p rental.book.title }
-# book.rentals.map { |rental| p rental.person.name }
-# p book.rentals.count
