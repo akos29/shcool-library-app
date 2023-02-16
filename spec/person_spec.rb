a7606db7afb34918d6e75f6ea1478d1c807a27b4
jonathanmunamire <jonathanmunamire@gmail.com>
2023-02-16 17:10:54 +0200

    Fix Linters

--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -1,32 +1,32 @@
-require "./person"
+require './person'
 
 describe Person do
-    context 'testing Person class' do
-        it 'should return false if there is no parent permission' do
-            person1 = Person.new(1, 15, "John", parent_permission: false)
-            service = person1.can_use_services?
+  context 'testing Person class' do
+    it 'should return false if there is no parent permission' do
+      person1 = Person.new(1, 15, 'John', parent_permission: false)
+      service = person1.can_use_services?
 
-            expect(service).to eq(false)    
-        end
-
-        it 'should return true if there is no parent permission but is of age' do
-            person2 = Person.new(2, 20, "Jack", parent_permission: false)
-            service = person2.can_use_services?
-
-            expect(service).to eq(true)    
-        end
-
-        it 'should return the correct name' do
-            person3 = Person.new(3, 17, "Jane")
-            correct = person3.correct_name
-
-            expect(correct).to eq("Jane")
-        end
-
-        it 'should return if is of age' do
-            person4 = Person.new(4, 11, "Jessica")
-
-            expect(person4.send(:of_age?)).to eq(false)
-        end
+      expect(service).to eq(false)
     end
+
+    it 'should return true if there is no parent permission but is of age' do
+      person2 = Person.new(2, 20, 'Jack', parent_permission: false)
+      service = person2.can_use_services?
+
+      expect(service).to eq(true)
+    end
+
+    it 'should return the correct name' do
+      person3 = Person.new(3, 17, 'Jane')
+      correct = person3.correct_name
+
+      expect(correct).to eq('Jane')
+    end
+
+    it 'should return if is of age' do
+      person4 = Person.new(4, 11, 'Jessica')
+
+      expect(person4.send(:of_age?)).to eq(false)
+    end
+  end
 end
