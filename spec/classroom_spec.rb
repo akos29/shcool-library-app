4d44092babc4c9f9caf02e3c5059cf1481245b12
LAPTOP-4QVU197N\KB <kibrish49@gmail.com>
2023-02-16 14:26:06 +0200

    Fix Rspec error

--- spec/classroom_spec.rb
+++ spec/classroom_spec.rb
@@ -1,6 +1,6 @@
 require './classroom'
 
-RSpec.describe "Classroom" do
+RSpec.describe 'Classroom' do
   context 'Testing Classroom class' do
     it 'should create a classroom with lable and empty array', positive: true do
       class_room = Classroom.new 'B121 F102'
@@ -11,11 +11,11 @@ RSpec.describe "Classroom" do
 
     it 'should not create a classroom when lable is empty', negative: true do
       expect { Classroom.new }.to raise_error(ArgumentError)
-    end 
+    end
 
     # it 'should add rentals when add_rental method is called' do
     #   class_room = Classroom.new 'B117'
-      
+
     #   class_room.add_student(["student1"])
 
     #   expect(class_room).to have_attributes(students: ["student1"])
