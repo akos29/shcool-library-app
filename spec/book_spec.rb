30d871eee99c664121dd76c6f0b465a5935b3c03
LAPTOP-4QVU197N\KB <kibrish49@gmail.com>
2023-02-18 21:40:07 +0200

    Update test case examples

--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -13,17 +13,5 @@ describe Book do
       expect { Book.new 'The Idiot' }.to raise_error(ArgumentError)
     end
 
-    # it 'should add rentals when add_rental method is called' do
-    #   book = Book.new title: 'The Idiot', author: 'Fyodor Dostoevsky'
-
-    #   person = double('person')
-
-    #   allow(person).to receive(:id) { 223 }
-
-    #   book.add_rental('2023-01-01', person)
-
-    #   # expect(book).to  be nil
-    #   expect(book).to have_attributes(rentals: ['2023-01-01'])
-    # end
   end
 end
